2837208605751dea6869e90286a2624c3be61c84
antoniofd20 <antonioflodiaz@gmail.com>
2021-01-13 14:46:10 -0600

    Variables

---
+++ 02/variables.php
@@ -0,0 +1,25 @@
+<?php
+
+    # STRING: CADENA DE TEXTO
+    $nombre = 'Raymundo Antonio Flores Díaz';
+
+    # INTEGER: NUMEROS ENTEROS
+    $numero = 34;
+
+    # DOUBLE: NUMEROS CON DECIMALES
+    $numero_decimal = 7.7;
+
+    # BOOLEAN: VERDADERO O FALSO ( TRUE/FALSE)
+    $verdadero = true;
+
+    # ARRAY: ARREGLO
+    # OBJECT: OBJETO
+    # CLASS: CLASE
+    # NULL: CUANDO UNA VARIABLE AUN NO SE LE HA ASIGNADO NINGUN VALOR
+
+    echo 'Hola, ' . $nombre . ' ';
+
+    # CONOCER EL TIPO DE DATO DE UNA VARIABLE
+    echo gettype($nombre);
+
+?>
