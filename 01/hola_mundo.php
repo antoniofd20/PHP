e1e94b7279a00539468354ff8c015e0665d7053f
antoniofd20 <antonioflodiaz@gmail.com>
2021-01-13 14:28:37 -0600

    Introduccion a PHP, sintaxis básica

---
+++ 01/hola_mundo.php
@@ -0,0 +1,23 @@
+<?php
+
+    // echo SIRVE PARA MOSTRAR INFORMACION EN PANTALLA
+    # OTRA FORMA DE COMENTAR
+    /*
+        UNA MÁS!!
+    */
+
+    echo "Hola Mundo";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola mundo!</title>
+</head>
+<body>
+    <h1><?php echo "Esto es código PHP dentro de HTML";?></h1>
+</body>
+</html>
